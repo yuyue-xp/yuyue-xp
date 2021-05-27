4f322abb8c345a26fd7741e67511855f7cd81538
yuyue-xp <69074603+xjhw-Git@users.noreply.github.com>
2021-05-27 05:03:43 -0500

    Create 1.py

---
+++ download/1.py
@@ -0,0 +1,15 @@
+s=int(input("请定义S的值:"))
+p=int(input("请定义P的值:"))
+a=int(6)
+b=int(11)
+c=float(1.0)
+d=float(0.9)
+e=float(0.8)
+if s<a:
+ j=c
+if s>=a:
+ j=d
+if s>=b:
+ j=e
+t=s*p*j
+print("T的值为:",t,)
